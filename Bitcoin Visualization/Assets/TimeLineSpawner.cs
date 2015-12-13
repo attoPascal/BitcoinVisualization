535da0f68c753b23a8199ceb0851935d97884e94
Marcell Lanczos <MCL@Marcells-MacBook-Pro.local>
2015-12-13 20:30:54 +0100

    first reimplementation of TimeLineView

---
+++ Bitcoin Visualization/Assets/TimeLineSpawner.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using Bitcoin;
+using DAO;
+using System.IO;
+
+public class TimeLineSpawner : MonoBehaviour {
+
+	private ArrayList lines;
+	string dataPath = "";
+	BitcoinDAO input;
+
+	// Use this for initialization
+	void Start () {
+		if (Application.platform == RuntimePlatform.OSXPlayer || Application.platform == RuntimePlatform.OSXEditor) {
+			dataPath = @"../data/1000blocks/";
+		} else if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor) {
+			dataPath = @"..\data\1000blocks\";
+			Debug.Log ("detected Windows OS");
+		}
+		input = new CSVDAO(dataPath);
+		lines = new ArrayList ();
+		float halfBlocks = input.Blocks.Count/2f;
+		foreach (Block b in input.Blocks) {
+			GameObject line = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+			float blockTransactions = (float) b.Transactions.Count;
+			line.transform.localScale = new Vector3(0.2f, blockTransactions*2, 0.2f);
+			line.transform.position = new Vector3((b.Height-halfBlocks)*0.2f, -blockTransactions, 0);
+			line.GetComponent<MeshRenderer>().enabled=false;
+
+
+			Vector3 top = new Vector3((b.Height-halfBlocks)*0.2f, blockTransactions, 0);
+			Vector3 bottom = new Vector3((b.Height-halfBlocks)*0.2f, -blockTransactions, 0);
+
+			LineRenderer lRend = line.AddComponent<LineRenderer>() as LineRenderer;
+			lRend.material = new Material(Shader.Find("Particles/Additive"));
+			lRend.SetVertexCount(2);
+			lRend.SetColors(Color.red, Color.green);
+			lRend.SetPosition (0, top);
+			lRend.SetPosition(1, bottom);
+			lRend.SetWidth(0.2f,0.2f);
+
+			lines.Add (line);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
